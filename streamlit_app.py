13f2f88823cf231ec4a8b2841ef263986a1969a2
bvvmpc <56621670+bvvmpc@users.noreply.github.com>
2023-04-18 17:27:11 +0530

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Bluberry Oat meal')
+streamlit.text('kale, Spinach & Rocket smoothie')
